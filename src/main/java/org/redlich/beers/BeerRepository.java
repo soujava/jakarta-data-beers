dbfa4d6e757719a7ac9f28e0726f330b1e9737a4
Maximillian Arruda <dearrudam@gmail.com>
2024-02-04 05:38:31 -0300

    chore: fix repository methods
    Signed-off-by: Maximillian Arruda <dearrudam@gmail.com>

--- src/main/java/org/redlich/beers/BeerRepository.java
+++ src/main/java/org/redlich/beers/BeerRepository.java
@@ -2,21 +2,21 @@ package org.redlich.beers;
 
 import jakarta.data.page.Page;
 import jakarta.data.page.Pageable;
-import jakarta.data.repository.Repository;
 import jakarta.data.repository.DataRepository;
-import jakarta.data.repository.OrderBy;
-import jakarta.data.repository.Query;
 import jakarta.data.repository.Param;
+import jakarta.data.repository.Query;
+import jakarta.data.repository.Repository;
 import jakarta.data.repository.Save;
 import jakarta.validation.Valid;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Stream;
 
 @Repository
 public interface BeerRepository extends DataRepository<Beer, Integer> {
-    
+
     Stream<Beer> findAll();
 
     Set<Beer> findByName(String beer);
@@ -27,7 +27,9 @@ public interface BeerRepository extends DataRepository<Beer, Integer> {
 
     Page<Beer> findByBeer(String beer, Pageable pageRequest);
 
-    void deleteByBeerId(int beerId);
+    Optional<Beer> findById(int id);
+
+    void deleteById(int id);
 
     @Query("select * from Beer where name = @name")
     Set<Beer> query(@Param("name") String name);
@@ -35,6 +37,7 @@ public interface BeerRepository extends DataRepository<Beer, Integer> {
     @Save
     Beer save(@Valid Beer beer);
 
+    @Query("delete from Beer")
     void deleteAll();
-    }
+}
 
